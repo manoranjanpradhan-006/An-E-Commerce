509e43f7d25e98ec3403e65bf8a16a4d1662dfa7
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-09-16 15:21:36 +0530

    Refactor Navbar component styles and update App.jsx import statement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,44 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-black text-white">
+      <nav className="bg-orange-100 text-emerald-800">
         <div className="max-w-7xl mx-auto flex items-center justify-between py-6">
           <div className="nav-left flex items-center justify-between">
-            <div className="logo text-xl font-bold flex items-center gap-2 px-3">
+            <div className="logo text-2xl font-bold flex items-center gap-2 px-3">
               <h1>Your Commerce</h1>
             </div>
           </div>
-          <div className="nav-center flex gap-8 text-xl ">
-            <h1>Home</h1>
-            <h1>Shop</h1>
-            <h1>Categoies</h1>
-            <h1>Deals</h1>
-            <h1>About</h1>
+          <div className="nav-center flex gap-8 text-xl font-bold">
+            <a
+              href="/"
+              className="hover:text-orange-700 hover:scale-105 transition"
+            >
+              Home
+            </a>
+            <a
+              href="/Shop"
+              className="hover:text-orange-700 hover:scale-105 transition"
+            >
+              Shop
+            </a>
+            <a
+              href="/Products"
+              className="hover:text-orange-700 hover:scale-105 transition"
+            >
+              Products
+            </a>
+            <a
+              href="/Categories"
+              className="hover:text-orange-700 hover:scale-105 transition"
+            >
+              Categories
+            </a>
+            <a
+              href="/Deals"
+              className="hover:text-orange-700 hover:scale-105 transition"
+            >
+              Deals
+            </a>
           </div>
           <div className="nav-right flex items-center gap-5 text-xl px-3">
             <button>
